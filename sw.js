63c48f2123dd881596d5daae02796b99bd36b85a
Jungpil YU <31795854+jungpilyu@users.noreply.github.com>
2019-01-21 00:36:18 +0900

    [feature] Update sw.js

--- sw.js
+++ sw.js
@@ -1,60 +1,74 @@
-var cacheID = "mws-restaruant-001";
+const cacheID = 'mws-restaurant-001';
 
-self.addEventListener("install", event => {
+// list of assets to cache on install
+// cache each restaurant detail page as well
+self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(cacheID).then(cache => {
-      return cache
-        .addAll([
-          "/",
-          "/index.html",
-          "/restaurant.html",
-          "/css/styles.css",
-          "/data/restaurants.json",
-          "/js/",
-          "/js/dbhelper.js",
-          "/js/main.js",
-          "/js/restaurant_info.js",
-          "/img/na.png",
-          "/js/register.js"
-        ])
-        .catch(error => {
-          console.log("Caches open failed: " + error);
+    caches.open(cacheID)
+      .then(cache => {
+        return cache.addAll([
+          '/index.html',
+          // '/restaurant.html',
+          '/css/styles.css',
+          '/js/dbhelper.js',
+          '/js/register_sw.js',
+          '/js/main.js',
+          '/js/restaurant_info.js',
+          // '/data/restaurants.json',
+          '/restaurant.html?id=1',
+          '/restaurant.html?id=2',
+          '/restaurant.html?id=3',
+          '/restaurant.html?id=4',
+          '/restaurant.html?id=5',
+          '/restaurant.html?id=6',
+          '/restaurant.html?id=7',
+          '/restaurant.html?id=8',
+          '/restaurant.html?id=9',
+          '/restaurant.html?id=10',
+          '/img/na.png'
+        ]).catch(error => {
+          console.log('Caches open failed: ' + error);
         });
+      })
+  );
+});
+
+
+// intercept all requests
+// either return cached asset or fetch from network
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    // Add cache.put to cache images on each fetch
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request).then(fetchResponse => {
+        return caches.open(cacheID).then(cache => {
+          cache.put(event.request, fetchResponse.clone());
+          return fetchResponse;
+        });
+      });
+    }).catch(error => {
+      if (event.request.url.includes('.jpg')) {
+        return caches.match('/img/na.png');
+      }
+      return new Response('Not connected to the internet', {
+        status: 404,
+        statusText: "Not connected to the internet"
+      });
     })
   );
 });
 
-self.addEventListener("fetch", event => {
-  let cacheRequest = event.request;
-  let cacheUrlObj = new URL(event.request.url);
-  if (event.request.url.indexOf("restaurant.html") > -1) {
-    const cacheURL = "restaurant.html";
-    cacheRequest = new Request(cacheURL);
-  }
-  if (cacheUrlObj.hostname !== "localhost") {
-    event.request.mode = "no-cors";
-  }
-
-  event.respondWith(
-    caches.match(cacheRequest).then(response => {
-      return (
-        response ||
-        fetch(event.request)
-          .then(fetchResponse => {
-            return caches.open(cacheID).then(cache => {
-              cache.put(event.request, fetchResponse.clone());
-              return fetchResponse;
-            });
-          })
-          .catch(error => {
-            if (event.request.url.indexOf(".jpg") > -1) {
-              return caches.match("/img/na.png");
-            }
-            return new Response("Application is not connected to the internet", {
-              status: 404,
-              statusText: "Application is not connected to the internet"
-            });
-          })
+// delete old/unused static caches
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    // caches.delete('-restaurant-static-001')
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.filter(cacheName => {
+          return cacheName.startsWith('mws-restaurant-') && cacheName !== cacheID;
+        }).map(cacheName => {
+          return caches.delete(cacheName);
+        })
       );
     })
   );
